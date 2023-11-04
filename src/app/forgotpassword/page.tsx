60af199d4a22a2c8b3267fcbc07828e215f331a6
kartikscript <kartik.13102003@gmail.com>
2023-11-04 23:35:14 +0530

    completed

--- src/app/forgotpassword/page.tsx
+++ src/app/forgotpassword/page.tsx
@@ -1,48 +1,59 @@
 "use client";
 
 import axios from "axios";
-import { useState ,} from "react";
-import { useRouter } from 'next/navigation';
-
-
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function forgotpassword() {
-
   const [email, setEmail] = useState("");
-  const [token, setToken]=useState('');
-  const [buttonDisabled, setButtonDisabled]=useState(false)
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
-  const router=useRouter()
+  const router = useRouter();
 
-  const sendToken =async function(){
-    try{
-    console.log(email)
+  const sendToken = async function () {
+    try {
+      console.log(email);
 
-     await axios.post('/api/users/forgotpassword', {email})
-     
-console.log('sent res')
-    }catch(err:any){
-        console.log(err.message)
+      await axios.post("/api/users/forgotpassword", { email });
+      setButtonDisabled(true);
+      console.log("sent res");
+    } catch (err: any) {
+      console.log(err.message);
     }
-  
-  }
-
-
- 
-
+  };
 
   return (
-    <div>
-      <label htmlFor="enter your email">enter your email</label>
-      <input
-        id="enter your email"
-        type="text"
-        placeholder="email"
-        value={email}
-        onChange={(e)=>setEmail(e.target.value)}
-      />
-      <button onClick={sendToken}>Send token</button>
- 
+    <div className="h-screen capitalize flex justify-center items-center bg-gradient-to-br from-sky-700 to-green-300 ">
+      <div className="w-4/6 h-5/6 flex  items-center justify-between bg-white/50 rounded-tr-[30%] rounded-bl-[30%]">
+        <h1 className="z-10 text-8xl self-start m-6  text-white/40 border-r-2 border-b-2 border-r-black/10 border-b-black/10 pb-4">
+          Get Token
+        </h1>
+        <div className="justify-self-start border-l-2 border-t-2 border-l-black/10 border-t-black/10 p-4">
+          <label className="text-xl text-teal-700" htmlFor="enter your email">
+            enter your email
+          </label>
+          <input
+            className="p-1 my-5  border border-gray-300 rounded-lg  focus:outline-none focus:border-gray-600 text-black"
+            id="enter your email"
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <button
+            className="p-2 text-base bg-emerald-400 text-white/60 "
+            onClick={sendToken}
+          >
+            Send token
+          </button>
+          {buttonDisabled && (
+            <h2 className="text-2xl text-center p-2 text-green-700 bg-green-300">
+              token sent!!
+            </h2>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/resetpassword/page.tsx
+++ src/app/resetpassword/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 
 import axios from "axios";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react"
 
 export default function resetpassword() {
   const [token, setToken] = useState("");
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm]=useState('')
-  const [ischanged,setIsChanged] = useState(false)
+
+
   const router=useRouter()
 
   const changePassword = async () => {
@@ -16,6 +17,7 @@ export default function resetpassword() {
       if(password===passwordConfirm){
       await axios.post("/api/users/resetpassword", { token ,password});
       }
+router.push('/login')
       
     } catch (error: any) {
       console.log(error.response.data);
@@ -29,25 +31,33 @@ export default function resetpassword() {
 
  
 return(
-  <div>
-      <label htmlFor="password">enter new password</label>
+<div className="h-screen capitalize flex  justify-center items-center bg-gradient-to-br from-sky-700 to-green-300 ">
+      <div className="w-4/6 h-5/6 flex flex-col items-center  bg-white/50 rounded-tr-[30%] rounded-bl-[30%]">
+        <h1 className="z-10 text-8xl self-start m-6  text-white/40 border-r-2 border-b-2 border-r-black/10 border-b-black/10 pb-4">
+          Password<br/> Change
+        </h1>
+        <div className="border-2   border-r-0 border-b-0 border-black/10   p-12">   
+           <label className="text-xl text-teal-700" htmlFor="password">enter new password</label>
       <input
+        className="p-1 my-5  border border-gray-300 rounded-lg  focus:outline-none focus:border-gray-600 text-black"
       id='password'
       placeholder="new password"
       value={password}
       onChange={(e)=>setPassword(e.target.value)}
       />
       <br/>
-           <label htmlFor="confirm password"> confirm password</label>
+           <label className="text-xl text-teal-700" htmlFor="confirm password"> confirm password</label>
       <input
+        className="p-1 my-5  border border-gray-300 rounded-lg  focus:outline-none focus:border-gray-600 text-black"
       id='confirm password'
       placeholder="confirm password"
       value={passwordConfirm}
       onChange={(e)=>setPasswordConfirm(e.target.value)}
       />
       <br/>
-      <button onClick={changePassword}>change password</button>
-      <p>{ischanged?"password changed" :""}</p>
+      <button className="p-2 mx-auto text-base bg-emerald-400 text-white/60 " onClick={changePassword}>change password</button>
+  </div>
+  </div>
   </div>
 )
 
